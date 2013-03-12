7a8db090e9d1441fea76bef5c55390427f675273
Loren Davie <loren@axilent.com>
2013-03-12 09:29:18 -0400

    Adding sync lock handling to sync_axilent management command.

--- djax/management/commands/sync_axilent.py
+++ djax/management/commands/sync_axilent.py
@@ -13,4 +13,11 @@ class Command(BaseCommand):
         """
         Handler method.
         """
-        sync_content()
+        print 'Syncing local models with Axilent'
+        result = sync_content()
+        if result:
+            print 'Content model has been synced with Axilent'
+        else:
+            print '''Local model is sync-locked, suggesting a concurrent sync is underway.  
+                     If you think this is an error, clear the lock by running "manage.py 
+                     clear_content_sync_locks".'''
