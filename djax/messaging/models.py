43caad55d759b6d67c19ae4867d756c391883751
Loren Davie <loren@axilent.com>
2013-05-14 11:49:38 -0400

    Unpacking list

--- djax/messaging/models.py
+++ djax/messaging/models.py
@@ -78,7 +78,7 @@ class Recipient(models.Model):
         
         if filters:
             local_ids = []
-            for message_content_type in ContentType.objects.get_for_models(message_models).values():
+            for message_content_type in ContentType.objects.get_for_models(*message_models).values():
                 local_ids += [message_object.pk for message_object in message_content_type.objects.filter(**filters)]
             
             messages = messages.filter(local_id__in=local_ids)
