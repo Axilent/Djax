cb295d74b136834025a88b409796ef6797a93e1f
Loren Davie <loren@axilent.com>
2014-03-02 11:58:21 -0500

    Now accepting sync-record url.

--- djax/urls.py
+++ djax/urls.py
@@ -4,5 +4,5 @@ URLs for Djax.
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('djax.views',
-    url(r'^djax/(?P<token>[a-fA-F0-9]+)/$','phone_home'),
+    url(r'^sync-record/$','sync_record'),
 )
