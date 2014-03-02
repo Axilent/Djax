c24da35bf3b55976f9068e3bba7dbfe5eb6fc2af
Loren Davie <loren@axilent.com>
2014-03-02 12:24:42 -0500

    Now using correct init arg for http response status code.

--- djax/views.py
+++ djax/views.py
@@ -28,7 +28,7 @@ def check_auth(view):
                         origin_domain = request.META.get('REMOTE_HOST',None)
                         if not origin_domain == auth_token.origin_domain:
                             print 'non-matching origin domain'
-                            return HttpResponse('Not Allowed',status_code=403)
+                            return HttpResponse('Not Allowed',status=403)
                 
                     print 'auth token is good, running target view'
                     return view(request,*args,**kwargs)
@@ -37,7 +37,7 @@ def check_auth(view):
                     pass
         
             print 'request is not authorized'
-            return HttpResponse('Not Allowed',status_code=403)
+            return HttpResponse('Not Allowed',status=403)
     
     return wrapper
 
@@ -56,8 +56,8 @@ def sync_record_view(request):
     content_key = request.GET.get('content_key',None)
     if content_type and content_key:
         if sync_record(content_type,content_key):
-            return HttpResponse('Created %s:%s' % (content_type,content_key),status_code=201)
+            return HttpResponse('Created %s:%s' % (content_type,content_key),status=201)
         else:
             return HttpResponse('Updated %s:%s' % (content_type,content_key))
     else:
-        return HttpResponse('Badly formed request, specify content_type and content_key in request params.',status_code=409)
+        return HttpResponse('Badly formed request, specify content_type and content_key in request params.',status=409)
