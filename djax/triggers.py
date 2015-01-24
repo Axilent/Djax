0d424ac1c6bf6eb063ac6d25c37e4d3b3409f00d
Loren Davie <code@axilent.com>
2015-01-24 17:11:58 -0500

    Adding debugging

--- djax/triggers.py
+++ djax/triggers.py
@@ -34,6 +34,7 @@ def build_mappings():
                     trigger_list = getattr(module,'triggers')
                     import_triggers(trigger_list)
             except ImportError:
+                log.exception('Import Error')
                 log.warn('Cannot import %s.triggermap. Skipping' % app_path)
 
 def import_triggers(trigger_list):
