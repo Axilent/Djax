dbe653107af1e230b6107c3fab1e61972e13adff
Loren Davie <code@axilent.com>
2015-06-21 13:57:27 -0400

    Making import Django 1.8 compatible

--- djax/urls.py
+++ djax/urls.py
@@ -1,7 +1,10 @@
 """
 URLs for Djax.
 """
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
 
 urlpatterns = patterns('djax.views',
     url(r'^sync-record/$','sync_record_view'),
